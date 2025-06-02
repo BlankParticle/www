69fc0fb406fddcdbf07a51eecf0f51b7c8f858e7
BlankParticle <blankparticle@gmail.com>
2025-06-02 18:31:07 +0530

    chore: fix pfp

--- src/routes/me.[ext]/+server.ts
+++ src/routes/me.[ext]/+server.ts
@@ -17,9 +17,6 @@ export const GET: RequestHandler = async ({ params }) => {
   switch (params.ext) {
     case "jpg":
     case "png":
-      return cacheProxy(
-        "https://cdn.discordapp.com/avatars/1096392763144159252/18f79f01ea651a5f4290f6d0d2712b78.webp?size=160",
-      );
     case "icon.png":
       return cacheProxy("https://github.com/BlankParticle.png");
   }
